82f85c3ed39fb2186830d5a82c9db6f64391c34e
jinfang <jinfang134@sina.com>
2019-11-28 00:10:23 +0800

    添加一个失败的测试用例

--- src/components/data.js
+++ src/components/data.js
@@ -34,13 +34,30 @@ const sample = {
         { name: "test3", time: '2m23s', status: 'success', next: [7] },
         { name: "test4 ha", time: '2m23s', status: 'failure', next: [8,9] },//4
         { name: "test5", time: '2m23s', status: 'failure', next: [6] },
-        { name: "test6", time: '2m23s', status: 'success', next: [7] },//6
+        { name: "test6", time: '2m23s', status: 'success', next: [10] },//6
         { name: "test7 hello", time: '2m23s', status: 'paused', next: [10] },
         { name: "test8 2344", time: '2m23s', status: 'paused', next: [10] },//8
         { name: "test9", time: '2m23s', status: 'failure',next:[10] },
-        { name: "test10", time: '2m23s', status: 'failure' },
+        { name: "test10", time: '2m23s', status: 'failure',next:[11] }, //10
+        { name: "test11", time: '2m23s', status: 'failure' },
+    ],
+}
+const sample2 = {
+    nodes: [
+        { name: "test0", time: '1m23s', status: 'success', next: [1] },
+        { name: "test1 long", time: '1m23s', status: 'success', next: [2, 3, 4] },
+        { name: "test2 hello world", time: '2m23s', status: 'unstable', next: [5] },
+        { name: "test3", time: '2m23s', status: 'success', next: [7] },
+        { name: "test4 ha", time: '2m23s', status: 'failure', next: [8,9] },//4
+        { name: "test5", time: '2m23s', status: 'failure', next: [6] },
+        { name: "test6", time: '2m23s', status: 'success', next: [10] },//6
+        { name: "test7 hello", time: '2m23s', status: 'paused', next: [10] },
+        { name: "test8 2344", time: '2m23s', status: 'paused', next: [9] },//8
+        { name: "test9", time: '2m23s', status: 'failure',next:[10] },
+        { name: "test10", time: '2m23s', status: 'failure',next:[11] }, //10
+        { name: "test11", time: '2m23s', status: 'failure' },
     ],
 }
 
-// module.exports= sample
-export default hue2
+module.exports= sample
+// export default hue2
