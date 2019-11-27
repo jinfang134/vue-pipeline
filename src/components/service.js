e7c5a7517f8deb85ca8e6a13ee5d867ab4398925
zuo_ji <zuo_ji@worksap.co.jp>
2019-11-27 19:31:39 +0800

    temp save

--- src/components/service.js
+++ src/components/service.js
@@ -1,5 +1,9 @@
+/* eslint-disable no-console */
 
-const data =require('./data.js')
+const data = require('./data.js')
+
+const visited = []
+
 
 /**
  * 查找最长路径经过的节点
@@ -22,8 +26,82 @@ function findLongestWay(nodes, index) {
     return [index].concat(arr);
 }
 
-// function 
+function findParents(nodes, index) {
+    let arr = [];
+    for (let i = 0; i < nodes.length; i++) {
+        if (nodes[i].next && nodes[i].next.includes(index)) {
+            arr.push(i);
+        }
+    }
+    return arr;
+}
 
+/**
+ * 计算每个点的坐标
+ * @param {*} nodes 
+ * @param {*} start 起点的下标
+ * @param {Number} startx x轴起点
+ * @param {Number} starty y轴起点
+ * 
+ */
+function calculatePosition(nodes, start, startx, starty, xstep,ystep) {
+    let list = findLongestWay(nodes, start)
+    list.forEach((it, index) => {
+        visited[it] = true
+        nodes[it].x = startx + xstep * index
+        nodes[it].y = starty
+    })
+    let result = bfs(nodes, start, startx)
+
+
+
+    // from the (index)th node to bfs, set the x coordinate of the first to x
+    function bfs(nodes, index, x ) {
+        const queue = [];
+        const visited = [];
+        let result = []
+        let xlist = []; //在当前x坐标上的y坐标列表, 如{120:70,240:80}
+        let node = nodes[index];
+        node.x = x;
+        node.y = starty;
+        node.index = index;
+        queue.push(node);
+        result[index] = node;
+        while (queue.length > 0) {
+            let first = queue.shift();
+            let y = starty;
+            visited[first.index] = true;
+            if (!first.next) {
+                continue;
+            }
+            for (let i = 0; i < first.next.length; i++) {
+                let childIndex = first.next[i];
+                let child = nodes[childIndex];
+                if (visited[childIndex] == true) {
+                    //已经有坐标,则更新坐标
+                    child.x = Math.max(
+                        first.x + xstep,
+                        result[childIndex].x
+                    );
+                    console.log(childIndex, child.x, result[childIndex].x);
+                    continue;
+                }
+                child.x = first.x + xstep;
+                child.y = y;
+                y += ystep;
+                child.index = childIndex;
+                queue.push(child);
+                //   visited[childIndex] = true;
+                result[childIndex] = child;
+            }
+            console.log(xlist);
+        }
+        console.log("node list", result);
+        return result;
+    }
+
+
+}
 
 
 
