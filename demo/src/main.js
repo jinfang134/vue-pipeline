9ac5d7f50478a48f60cdad8b57e8d6d65e3516a8
zuo_ji <zuo_ji@worksap.co.jp>
2019-11-29 20:59:00 +0800

    update

--- demo/src/main.js
+++ demo/src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VuePipeline from '../../index'
+// import VuePipeline from '../../index'
+import VuePipeline from 'vue-pipeline'
 
 Vue.config.productionTip = false
 
