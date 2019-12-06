57b28d22907a24bad2ca92dd5b406368f787cb9f
zuo_ji <zuo_ji@worksap.co.jp>
2019-12-06 16:06:30 +0800

    update script

--- deploy-demo.sh
+++ deploy-demo.sh
@@ -5,6 +5,6 @@ cd demo && yarn \
 && rm -rf /tmp/dist && mv -f dist /tmp/dist \
 && cd .. \
 && git checkout gh-pages \
-&& rm -rf ./* \
+&& rm -rf css fonts img js \
 && mv -f  /tmp/dist/* ./ \
 && git add . && git commit -m "update demo page" && git push origin gh-pages
