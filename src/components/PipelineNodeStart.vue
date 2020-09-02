eb17c93b0f430cb14bf7e200e4e50b24ed9be765
Animesh <animesh.workplace@gmail.com>
2020-09-02 18:38:07 +0530

    changed start rectangle to circle and shifted text up

--- src/components/PipelineNodeStart.vue
+++ src/components/PipelineNodeStart.vue
@@ -1,7 +1,8 @@
 <template>
   <g class="pipeline-node-start">
-    <rect rx="13" id="svg_1" height="26" width="40" y="-13" x="-20" stroke-width="1.5" />
-    <text id="svg_2" x="-13" y="3" class="pipeline-node-start__text">{{label}}</text>
+    <!-- <rect rx="13" id="svg_1" height="26" width="40" y="-13" x="-20" stroke-width="1.5" /> -->
+    <circle cx="0" cy="0" r="12" id="svg_1"></circle>
+    <text id="svg_2" x="-15" y="-20" class="pipeline-node-start__text">{{label}}</text>
   </g>
 </template>
 <script>
